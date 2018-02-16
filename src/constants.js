1c28fe7d3613b12c05a69d87d4426e5120feda30
Victor Perron <victor@iso3103.net>
2018-02-17 00:48:36 +0100

    Configure the port

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ module.exports = {
   MSG_INIT: 'init',
   MSG_REGION: 'region',
   REFRESH_RATE: 40,  // in milliseconds
-  WEBSOCKET_PORT: 8080
+  WEBSOCKET_PORT: process.env.LISTEN_PORT || 8080
 }
