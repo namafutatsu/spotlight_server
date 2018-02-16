3f973db4916bb32c7e7537de21e3a0e7a1fae2bd
Victor Perron <victor@iso3103.net>
2018-02-16 23:40:51 +0100

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+module.exports = {
+  IMAGE_PATH: '/tmp/spotlight_images',
+  MSG_COORDS: 'coords',
+  MSG_INIT: 'init',
+  MSG_REGION: 'region',
+  REFRESH_RATE: 40,  // in milliseconds
+  WEBSOCKET_PORT: 8080
+}
